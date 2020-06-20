abdc279fa71678ace9cb4ad18487bcae4f7e58c6
605794577 <605794577@qq.com>
2020-06-20 23:55:04 +0800

    Site updated: 2020-06-20 23:55:03

--- dist/music.js
+++ dist/music.js
@@ -14,7 +14,6 @@ const ap = new APlayer({
             artist: '许嵩',
             url: 'https://sharefs.yun.kugou.com/202006201159/6a211106e7925d17c1a93f2b59af3ce0/G123/M09/19/00/G4cBAFsnQUeAMrb5AD5yEvBAAJY536.mp3',
             cover: 'https://p3fx.kgimg.com/stdmusic/20180612/20180612032745754576.jpg',
-            lrc: 'lrc1.lrc',
             theme: '#ebd0c2'
         },
 		{
@@ -22,7 +21,6 @@ const ap = new APlayer({
             artist: '许嵩',
             url: 'https://sharefs.yun.kugou.com/202006191402/6c8c67c587dff1d7345d082894acbadb/G064/M09/10/08/IJQEAFc-aueAZCXaAEFnB1LjkYU037.mp3',
             cover: 'http://i2.tiimg.com/721551/a32fbf3cd9595eef.jpg',
-			lrc: 'lrc1.lrc',
             theme: '#ebd0c2'
         },
 		{
@@ -30,7 +28,6 @@ const ap = new APlayer({
             artist: 'ZARD',
             url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_7604493&response=res&type=convert_url&',
             cover: 'http://i2.tiimg.com/721551/83b3377ef16499d6.jpg',
-            lrc: 'lrc1.lrc',
             theme: '#ebd0c2'
         },
 		{
@@ -38,7 +35,6 @@ const ap = new APlayer({
             artist: 'ZARD',
             url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_28685072&response=res&type=convert_url&',
             cover: 'http://i2.tiimg.com/721551/83b3377ef16499d6.jpg',
-            lrc: 'lrc1.lrc',
             theme: '#ebd0c2'
         },
 		{
@@ -46,7 +42,6 @@ const ap = new APlayer({
             artist: 'Jeff Williams、Casey Lee Williams',
             url: 'https://sharefs.yun.kugou.com/202006191357/d4690b88cddea0599947728aa6ff2391/G034/M09/01/04/Yg0DAFWhQ7OAVk7JAFDGDL2ffBY854.mp3',
             cover: 'http://i2.tiimg.com/721551/8119ef3654908761.png',
-            lrc: 'lrc1.lrc',
             theme: '#ebd0c2'
         },
 	    {
@@ -54,7 +49,6 @@ const ap = new APlayer({
             artist: 'Jeff Williams、Casey Lee Williams',
             url: 'https://sharefs.yun.kugou.com/202006191418/c4983b3ae6df4dc52478d5a4997aea8f/G204/M06/17/13/rJQEAF50Ni-AStuQAD977NpPlzA483.mp3',
             cover: 'http://i1.fuimg.com/721551/6374c1d333b87e84.jpg',
-            lrc: 'lrc1.lrc',
             theme: '#ebd0c2'
         },
         {
@@ -62,7 +56,6 @@ const ap = new APlayer({
             artist: 'Jeff Williams、Casey Lee Williams',
             url: 'https://sharefs.yun.kugou.com/202006191323/adb94f04a79d152fc59b25b57a18d37a/G111/M02/16/00/D4cBAFlEXo-AStsxADEXr1sKdUg397.mp3',
             cover: 'http://i1.fuimg.com/721551/e823f59c04111e6a.png',
-            lrc: 'lrc2.lrc',
             theme: '#46718b'
         }
     ]
